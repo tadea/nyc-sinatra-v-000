6b3932fb1d8f3475f8179490024f22832a26670f
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-10-18 00:04:46 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,6 +2,6 @@ class LandmarksController < ApplicationController
 
     get '/landmarks' do 
       @landmarks = Landmark.all
-      erb :'/landmark/index'
+      erb :'/landmarks/index'
     end
 end
